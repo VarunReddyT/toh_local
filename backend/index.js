4926bbee8a7ba0768268640a97b91119298fcba7
Varun Reddy <tvarun2014@gmail.com>
2024-03-04 11:41:31 +0530

    Minor

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const tollChRcImages = require('./routes/tollChRcImages.js');
 const statistics = require('./routes/statistics.js')
 const feedback = require('./routes/feedback.js');
 const app = express();
-const dotenv = require('dotenv').config;
+require('dotenv').config();
 
 // ^ defining port
 const port = 4000;
@@ -24,6 +24,8 @@ app.use(cors({
 }));
 
 // & MongoDB connection
+// console.log(process.env.MONGO_URL);
+// mongoose.connect('mongodb+srv://g81psnew:G812023PSNew@g81.gevu1bu.mongodb.net/myFirst')
 mongoose.connect('mongodb://127.0.0.1:27017/myFirst')
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Error connecting to MongoDB:', err));
