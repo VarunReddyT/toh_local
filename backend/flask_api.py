64e2c789490aac51ed364fd9e609baa7c4f6538e
Varun Reddy <tvarun2014@gmail.com>
2024-03-04 20:11:23 +0530

    model

--- backend/flask_api.py
+++ backend/flask_api.py
@@ -12,6 +12,17 @@ CORS(app)
 model = keras.models.load_model('best_model.h5')
 
 def preprocess_image(image_path):
+    """
+    The `preprocess_image` function takes an image file path, resizes it to 64x64 pixels, converts it to
+    a NumPy array, normalizes the pixel values, and returns the preprocessed image.
+    
+    :param image_path: The `image_path` parameter is a string that represents the file path to the image
+    that you want to preprocess. This function loads the image from the specified path, resizes it to a
+    target size of 64x64 pixels, converts it to a NumPy array, expands its dimensions to include
+    :return: The function preprocess_image is returning a preprocessed image in the form of a NumPy
+    array with shape (1, 64, 64, 3) where the image has been resized to 64x64 pixels, converted to an
+    array, expanded dimensions to include batch size, and normalized by dividing by 255.0.
+    """
     img = image.load_img(image_path, target_size=(64, 64))
     img = image.img_to_array(img)
     img = np.expand_dims(img, axis=0)
@@ -26,6 +37,8 @@ def classify_image():
         if not files:
             return jsonify({"error": "No files uploaded"})
 
+# The code snippet you provided is part of a Flask API endpoint `/classify` that handles image
+# classification. Here's what the code is doing:
 
         for num, file in enumerate(files):
             image_path = f'temp_image_{num}.jpg'
--- backend/tires.py
+++ backend/tires.py
@@ -21,6 +21,9 @@ NUM_TEST_IMAGES = len(os.listdir(os.path.join(TEST_PATH,"cracked")))+len(os.list
 HEIGHT,WIDTH = 64,64
 BATCH_SIZE = 32
 SPLIT = 0.2
+# The `train_datagen` object created using `tf.keras.preprocessing.image.ImageDataGenerator` is used
+# for data augmentation during training of a neural network model. Here's what each parameter in
+# `train_datagen` is doing:
 train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
     rescale=1./255,
     rotation_range=30,
@@ -32,6 +35,8 @@ train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
     width_shift_range=0.2,
     height_shift_range=0.2,
     '''
+# `train_ds = train_datagen.flow_from_directory()` is creating a data generator for training data.
+# Here's what each parameter is doing:
 train_ds = train_datagen.flow_from_directory(
     MAIN_PATH,
     target_size = (HEIGHT,WIDTH),
@@ -41,6 +46,7 @@ train_ds = train_datagen.flow_from_directory(
     shuffle = True
 )
 
+# `val_ds = train_datagen.flow_from_directory()` is creating a data generator for validation data.
 val_ds = train_datagen.flow_from_directory(
     MAIN_PATH,
     target_size = (HEIGHT,WIDTH),
@@ -50,14 +56,27 @@ val_ds = train_datagen.flow_from_directory(
     shuffle = True
 )
 
+# The line `test_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)` is
+# creating an image data generator for the testing data. The `rescale=1./255` parameter is used to
+# normalize the pixel values of the images in the testing dataset. By dividing each pixel value by
+# 255, the pixel values are scaled to be in the range [0, 1], which is a common practice in deep
+# learning models to help with convergence and training stability.
 test_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
     rescale=1./255)
+# The code snippet `test_ds = test_datagen.flow_from_directory(TEST_PATH, target_size=(HEIGHT, WIDTH),
+# shuffle=False)` is creating an image data generator for the testing data.
 test_ds = test_datagen.flow_from_directory(
     TEST_PATH,
     target_size = (HEIGHT,WIDTH),
     shuffle = False
 )
 def create_model():
+    """
+    The function `create_model` builds a neural network model using VGG16 as a base with additional
+    layers for classification.
+    :return: The function `create_model()` returns a TensorFlow Keras model that consists of a VGG16
+    base model with additional layers for classification.
+    """
     vgg16 = tf.keras.applications.VGG16(include_top=False, weights='imagenet',input_shape=[HEIGHT,WIDTH, 3])
             
     x = vgg16.output
@@ -74,6 +93,21 @@ def create_model():
 model = create_model()
 model.summary()
 def compile_model(model, lr=0.000001):
+    """
+    The function `compile_model` compiles a given model with specified optimizer, loss function, and
+    metrics for training.
+    
+    :param model: The `compile_model` function takes a neural network model as input and compiles it
+    with specified optimizer, loss function, and metrics for training. The default learning rate is set
+    to 0.000001, but you can also specify a different learning rate if needed. The optimizer used is
+    Adam,
+    :param lr: The `lr` parameter in the `compile_model` function stands for the learning rate, which is
+    a hyperparameter that controls the step size during the optimization process of training a neural
+    network. It determines how much the model weights are updated during training based on the
+    calculated gradient
+    :return: The function `compile_model` is returning the compiled model with the specified optimizer,
+    loss function, and metrics.
+    """
     optimizer = Adam(learning_rate=lr)
     loss = CategoricalCrossentropy()
     metrics = [
@@ -84,6 +118,9 @@ def compile_model(model, lr=0.000001):
 model=compile_model(model, lr=0.0001)
 def create_callbacks():
     cpk_path = './best_model.h5'
+    # The `checkpoint = ModelCheckpoint()` function is creating a callback that will monitor the
+    # validation categorical accuracy during the training of the neural network model. Here's a
+    # breakdown of the parameters used in this function:
     checkpoint = ModelCheckpoint(
         filepath=cpk_path,
         monitor='val_categorical_accuracy',
@@ -91,6 +128,8 @@ def create_callbacks():
         save_best_only=True,
         verbose=1,
     )
+    # The `ReduceLROnPlateau` callback in TensorFlow is used to reduce the learning rate when a metric
+    # has stopped improving. Here's a breakdown of the parameters used in this callback:
     reducelr = ReduceLROnPlateau(
         monitor='val_categorical_accuracy',
         mode='max',
@@ -98,6 +137,9 @@ def create_callbacks():
         patience=5,
         verbose=1
     )
+    # The `EarlyStopping` callback is used to monitor a specified metric during the training of a
+    # neural network model and stop the training process early if the metric stops improving. Here's a
+    # breakdown of the parameters used in the `EarlyStopping` callback:
     earlystop = EarlyStopping(
         monitor='val_categorical_accuracy',
         mode='max',
@@ -111,6 +153,10 @@ VERBOSE =1
 
 # tf.keras.backend.clear_session()
 
+# The code block `with tf.device('/device:CPU:0'):` is specifying that the following operations within
+# the block should be executed on the CPU (Central Processing Unit) device with index 0. This can be
+# useful when you have multiple devices available (such as GPUs) and you want to explicitly choose to
+# run certain operations on a specific device.
 with tf.device('/device:CPU:0'):
     model = create_model()
     model = compile_model(model, lr=0.0001)
